9b11d48e88b6c6e78e563fa506c33d6b1e6e712d
homenwaterx86gcc <xaverhandl2@gmail.com>
2026-07-24 17:51:01 +0200

    UART wieder eingefügt, CLK speed geändert auf 10MHz

--- RiscGate/C/main.c
+++ RiscGate/C/main.c
@@ -1,28 +1,38 @@
-#include "gpio.h"
+//#include "gpio.h"
+#include "demo_system.h"
 //Versuche LED zu blinken
 #define GPIO_BASE_ADDRESS 0x80000000
+#define UART0_BASE 0x80001000
+#define UART_RX_REG 0
+#define UART_TX_REG 4
+#define UART_STATUS_REG 8
+
+#define UART_STATUS_RX_EMPTY 1
+#define UART_STATUS_TX_FULL 2
 // Inner loop iterations per "ms". The volatile counter costs ~10 cycles per
 // iteration, so at 10 MHz ~1000 iterations ~= 1 ms. The previous value (100000)
 // made one "ms" ~100 ms, i.e. ~100 s per half period - the LED looked dead.
+//Millers baudrate 4096
 #define CLK_SPEED 1000
 int main(void){
+    /*
     volatile uint32_t* pointer_to_gpio = (volatile uint32_t*)(GPIO_BASE_ADDRESS);
-    //Laut gpio.h ist gpio_out bei 0x0
-    volatile uint16_t delay_ms = 1000;
+    volatile uint32_t* uart_base = (volatile uint32_t*)(UART0_BASE);
+    char c = 'A';
+    volatile uint32_t* uart_tx_reg = (volatile uint32_t*)(UART0_BASE+UART_TX_REG);
+    volatile uint32_t* uart_status_reg = (volatile uint32_t*)(UART0_BASE + UART_STATUS_REG);
     while(1){
-        for(volatile uint16_t i = 0; i<delay_ms; i++){
-            for(volatile uint32_t j = 0; j<CLK_SPEED; ++j){
-                asm volatile("nop");
-            }
+        while(((*uart_status_reg >> UART_STATUS_TX_FULL) & 1 == 1)){
+            //mach ganix
         }
-        *pointer_to_gpio = 1;
-        for(volatile uint16_t i = 0; i<delay_ms; i++){
-            for(volatile uint32_t j = 0; j<CLK_SPEED; ++j){
-                asm volatile("nop");
-            }
-        }
-        *pointer_to_gpio = 0;
+        *uart_tx_reg = c;
+    }
+    */
 
-}
+    //Laut gpio.h ist gpio_out bei 0x0
+    //*pointer_to_gpio = 0;+
+    while(1){
+        puts("HAAAAALLLLLLLOOOOOOOOO");
+    }
 
 }
